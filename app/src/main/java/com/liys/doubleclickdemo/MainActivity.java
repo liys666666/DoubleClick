f208701594604a82715ee7458a9cfba9e1d1b3fc
littlecurl <424171723@qq.com>
2020-02-29 18:53:57 +0800

    迁移到AndroidX，更新Gradle版本

--- app/src/main/java/com/liys/doubleclickdemo/MainActivity.java
+++ app/src/main/java/com/liys/doubleclickdemo/MainActivity.java
@@ -2,8 +2,8 @@ package com.liys.doubleclickdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
--- app/src/main/java/com/liys/doubleclickdemo/fragment/MainActivity2.java
+++ app/src/main/java/com/liys/doubleclickdemo/fragment/MainActivity2.java
@@ -1,11 +1,11 @@
 package com.liys.doubleclickdemo.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
